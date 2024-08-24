a60f4e70e47324955daf7e7200380d23edbc431d
manvishw <vishwakarmamanas555@gmail.com>
2024-08-24 10:48:49 +0530

    Functions getComputerChoice() and getHumanChoice()
    getHumanChoice() => user input
    getComputerChoice() => computer decision randomly

--- logic.js
+++ logic.js
@@ -1,2 +1,12 @@
 let humanScore = 0,
   computerScore = 0;
+
+function getComputerChoice() {
+  const gameLogic = ["rock", "paper", "scissor"];
+  return gameLogic[Math.floor(Math.random() * 3)];
+}
+
+function getHumanChoice() {
+  const userInput = prompt("Enter your Decision");
+  return userInput.toLowerCase();
+}
