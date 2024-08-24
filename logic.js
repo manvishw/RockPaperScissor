296599fbebff04beddcb50758f32c28dfa1663c4
manvishw <vishwakarmamanas555@gmail.com>
2024-08-24 10:51:50 +0530

    Define function playRound()
    That consist Main Game Logic

--- logic.js
+++ logic.js
@@ -10,3 +10,32 @@ function getHumanChoice() {
   const userInput = prompt("Enter your Decision");
   return userInput.toLowerCase();
 }
+
+function playRound() {
+  const humanChoice = getHumanChoice();
+  const computerChoice = getComputerChoice();
+
+  if (humanChoice === computerChoice) {
+    console.log("Same");
+  } else if (humanChoice === "rock" && computerChoice === "scissor") {
+    humanScore += 1;
+    console.log("Human Wins");
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore += 1;
+    console.log("Computer Wins");
+  } else if (humanChoice === "paper" && computerChoice === "scissor") {
+    computerScore += 1;
+    console.log("Computer Wins");
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore += 1;
+    console.log("Human Wins");
+  } else if (humanChoice === "scissor" && computerChoice === "rock") {
+    computerScore += 1;
+    console.log("Computer Wins");
+  } else if (humanChoice === "scissor" && computerChoice === "paper") {
+    humanScore += 1;
+    console.log("Human Wins");
+  } else {
+    console.log('Please Select Only ["rock", "paper", "scissor"]');
+  }
+}
